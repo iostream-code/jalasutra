59a3950f0d7a1949e670a3b59cc13f54855ff613
iostream-code <aryakmifjannah2603@gmail.com>
2024-02-15 09:27:12 +0700

    Add laravel/passport module in jalasutra-api

---
+++ jalasutra-api/app/Http/Controllers/Api/Auth/LogoutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $removeToken = $request->user()->tokens()->delete();
+
+        if ($removeToken) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Logout Berhasil!',
+            ]);
+        }
+    }
+}
