fdf78b8b784b4b763b8e2f714e3e14f5802ec722
iostream-code <aryakmifjannah2603@gmail.com>
2024-02-16 20:47:11 +0700

    Add login page for admin

--- jalasutra-admin/src/components/partial/Sidebar.jsx
+++ jalasutra-admin/src/components/partial/Sidebar.jsx
@@ -1,3 +1,6 @@
+import { Link } from "react-router-dom"
+import { FaBuilding, FaRegNewspaper, } from "react-icons/fa"
+import { RiCustomerService2Fill } from "react-icons/ri";
 import Img from "../../assets/logo/logo-black.png"
 
 const Sidebar = () => {
@@ -18,7 +21,7 @@ const Sidebar = () => {
                             </a>
                         </div>
                         <div className="flex items-center">
-                            <div className="flex items-center ms-3">
+                            {/* <div className="flex items-center ms-3">
                                 <div>
                                     <button type="button" className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                         <span className="sr-only">Open user menu</span>
@@ -46,7 +49,10 @@ const Sidebar = () => {
                                         </li>
                                     </ul>
                                 </div>
-                            </div>
+                            </div> */}
+                            <Link to="/login" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                Login
+                            </Link>
                         </div>
                     </div>
                 </div>
@@ -62,6 +68,12 @@ const Sidebar = () => {
                                 </svg>
                                 <span className="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
                                 <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/layanan" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <RiCustomerService2Fill className=" w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                                <span className="flex-1 ms-3 whitespace-nowrap">Layanan</span>
                             </a>
                         </li>
                         <li>
@@ -83,10 +95,14 @@ const Sidebar = () => {
                         </li>
                         <li>
                             <a href="/desa" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                            <svg class="w-5 h-5 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-    <path fill-rule="evenodd" d="M4 4c0-.6.4-1 1-1h14a1 1 0 1 1 0 2v14a1 1 0 1 1 0 2H5a1 1 0 1 1 0-2V5a1 1 0 0 1-1-1Zm5 2a1 1 0 0 0-1 1v1c0 .6.4 1 1 1h1c.6 0 1-.4 1-1V7c0-.6-.4-1-1-1H9Zm5 0a1 1 0 0 0-1 1v1c0 .6.4 1 1 1h1c.6 0 1-.4 1-1V7c0-.6-.4-1-1-1h-1Zm-5 4a1 1 0 0 0-1 1v1c0 .6.4 1 1 1h1c.6 0 1-.4 1-1v-1c0-.6-.4-1-1-1H9Zm5 0a1 1 0 0 0-1 1v1c0 .6.4 1 1 1h1c.6 0 1-.4 1-1v-1c0-.6-.4-1-1-1h-1Zm-3 4a2 2 0 0 0-2 2v3h2v-3h2v3h2v-3a2 2 0 0 0-2-2h-2Z" clip-rule="evenodd"/>
-    </svg>
+                                <FaBuilding className=" w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                                 <span className="flex-1 ms-3 whitespace-nowrap">Desa</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/berita" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                <FaRegNewspaper className=" w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
+                                <span className="flex-1 ms-3 whitespace-nowrap">Berita</span>
                             </a>
                         </li>
                         <li>
@@ -95,16 +111,6 @@ const Sidebar = () => {
                                     <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
                                 </svg>
                                 <span className="flex-1 ms-3 whitespace-nowrap">Sign In</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <svg className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                                    <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                                    <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
-                                </svg>
-                                <span className="flex-1 ms-3 whitespace-nowrap">Sign Up</span>
                             </a>
                         </li>
                     </ul>
--- jalasutra-admin/src/views/Dashboard.jsx
+++ jalasutra-admin/src/views/Dashboard.jsx
@@ -1,3 +1,9 @@
+import Pic1 from "../assets/dashboard/information.png"
+import Pic2 from "../assets/dashboard/letter.png"
+import Pic3 from "../assets/dashboard/person.png"
+import Pic4 from "../assets/dashboard/office.png"
+import Pic5 from "../assets/dashboard/news.png"
+
 const Dashboard = () => {
     return (
         <>
@@ -7,17 +13,30 @@ const Dashboard = () => {
                     <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Jalasutra</h1>
                     <p className="mb-6 text-md font-normal text-gray-500 dark:text-gray-400">Jenis Apapun Layanan Cukup di Kantor Kecamatan atau Desa</p>
                     <div className="grid gap-2 text-gray-700 hover:text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3">
-                        <a href="/" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
-                            <div className="font-semibold mb-1">Layanan</div>
-                            <span className="text-sm text-gray-500">Cari layanan yang Anda butuhkan.</span>
+                        <a href="/layanan" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
+                            <img src={Pic1} className="w-1/6 mb-2" alt="" />
+                            <div className="font-semibold">Layanan</div>
+                            <span className="text-sm text-gray-500">Lihat layanan yang Anda butuhkan.</span>
                         </a>
-                        <a href="/" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
-                            <div className="font-semibold mb-1">Surat</div>
-                            <span className="text-sm text-gray-500">Lihat daftar surat yang Anda ajukan.</span>
+                        <a href="/surat" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
+                            <img src={Pic2} className="w-1/6 mb-2" alt="" />
+                            <div className="font-semibold">Surat</div>
+                            <span className="text-sm text-gray-500">Lihat daftar surat yang tersedia.</span>
                         </a>
-                        <a href="/" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
-                            <div className="font-semibold mb-1">Profile</div>
-                            <span className="text-sm text-gray-500">Atur data profile Anda.</span>
+                        <a href="/pengguna" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
+                            <img src={Pic3} className="w-1/6 mb-2" alt="" />
+                            <div className="font-semibold">Pengguna</div>
+                            <span className="text-sm text-gray-500">Lihat pengguna yang terdaftar.</span>
+                        </a>
+                        <a href="/desa" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
+                            <img src={Pic4} className="w-1/6 mb-2" alt="" />
+                            <div className="font-semibold">Desa</div>
+                            <span className="text-sm text-gray-500">Lihat desa yang terdaftar.</span>
+                        </a>
+                        <a href="/berita" className="block p-3 rounded-lg border border-collapse border-blue-200 transition-colors ease-in-out delay-75 duration-300 hover:bg-blue-200 hover:border-blue-400">
+                            <img src={Pic5} className="w-1/6 mb-2" alt="" />
+                            <div className="font-semibold">Berita</div>
+                            <span className="text-sm text-gray-500">Lihat berita yang tersedia.</span>
                         </a>
                     </div>
                 </div>
